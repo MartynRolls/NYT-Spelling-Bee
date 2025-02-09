b3058ddc49651d8781c430b656f58425d44168f0
MartynRolls <162859424+MartynRolls@users.noreply.github.com>
2025-02-09 21:25:18 +1300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,6 +1,5 @@
 from math import sin, cos, radians
 import pygame
-from pygame import gfxdraw
 
 HEXAGON_RADIUS = 10
 HEXAGON_SEPERATION = 1.5
@@ -36,16 +35,40 @@ class Tile:
         scaler = min(window_size[0], window_size[1]) * 0.01              # Calculate how large it should be
         x_offset, y_offset = window_size[0] * 0.5, window_size[1] * 0.6  # Calculate its position on the screen
 
-        draw_positions = [(int((self.x + self.size * x) * scaler + x_offset),
-                           int((self.y + self.size * y) * scaler + y_offset))
-                          for x, y in self.points]                       # Calculate all six points of the hexagon
+        hexagon_corners = [(int((self.x + self.size * x) * scaler + x_offset),
+                            int((self.y + self.size * y) * scaler + y_offset))
+                           for x, y in self.points]  # Calculate all six points of the hexagon
 
-        pygame.draw.polygon(surface, self.colour, draw_positions)        # Draw the hexagon onto the surface
+        pygame.draw.polygon(surface, self.colour, hexagon_corners)        # Draw the hexagon onto the surface
+
+        if self.animation_step > 0:
+            self.size = HEXAGON_RADIUS * (1 + 0.003 * self.animation_step * (self.animation_step - 15))
+            self.animation_step += 1
+            if self.animation_step == 16:
+                self.animation_step = 0
 
         # TODO: write letter on tile
 
         return surface
 
+    def check(self, window_size: tuple[int, int], mouse_position: tuple[int, int]) -> str:
+        mouse_x, mouse_y = mouse_position
+        window_x, window_y = window_size
 
-def create_tiles(letters: list[str]) -> list[Tile]:  # Create a tile for all seven letters
-    return [Tile(letters[i], start_positions[i]) for i in range(7)]
+        scaler = min(window_x, window_y) * 0.01              # Calculate how large the tile is
+        x_offset, y_offset = window_x * 0.5, window_y * 0.6  # Calculate its position on the screen
+
+        hexagon_corners = [(int((self.x + self.size * x) * scaler + x_offset),
+                            int((self.y + self.size * y) * scaler + y_offset))
+                           for x, y in self.points]  # Calculate all six points of the hexagon
+
+        if (hexagon_corners[0][0] > mouse_x > hexagon_corners[3][0] and
+                hexagon_corners[1][1] > mouse_y > hexagon_corners[4][1]):
+            self.animation_step = 1
+            return self.letter
+
+        return '-'
+
+
+def create_tiles(letters: list[str]) -> dict[str, Tile]:  # Create a tile for all seven letters
+    return {letters[i]: Tile(letters[i], start_positions[i]) for i in range(7)}
